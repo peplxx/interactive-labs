3d58ce6d7caf4ad9ee39f75912de3fc0b6e4f99b
sh3b0 <ahmedshaban784@gmail.com>
2025-09-15 19:15:17 +0300

    Terminal enhancements

--- terminal/scripts/start.sh
+++ terminal/scripts/start.sh
@@ -10,3 +10,4 @@ mkdir -p /home/dev/recordings
 # Use Moscow time (UTC+3) for the recording filename
 DATE_TIME=$(TZ=Europe/Moscow date '+%Y-%m-%d_%H-%M-%S')
 asciinema rec -i 1 -c "zellij attach --create $USERNAME" "/home/dev/recordings/$DATE_TIME.cast"
+asciinema upload "/home/dev/recordings/$DATE_TIME.cast"
